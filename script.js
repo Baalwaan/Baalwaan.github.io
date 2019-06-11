61d3db8fcb0c4102690b388ddb24d7817ee64915
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-06-12 00:55:59 +0100

    anchortag fixing attempt

---
+++ script.js
@@ -0,0 +1,14 @@
+const aboutMe = document.getElementById('about-me');
+const addPadding = e => {
+  aboutMe.style.marginBottom = '-50px';
+};
+aboutMe.addEventListener('click', addPadding);
+
+// {
+/* <a href="#second-pane" id="about-me">About Me</a>
+<a href="#third-pane" id="projects">Projects</a>
+<a href="/" id="contact-me">Contact Me</a>
+
+<section class="second-pane" id="second-pane">     
+ <section class="third-pane" id="third-pane"> */
+// }
