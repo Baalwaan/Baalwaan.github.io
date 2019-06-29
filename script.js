c6fd1ed0d2eb3fb2bef9cd4081cc8c65b0db2d65
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-06-29 19:27:42 +0100

    show form message when form is send

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ const toggleBurgerNav = () => {
 };
 
 links.forEach(link => link.addEventListener('click', toggleBurgerNav));
+
+document.getElementById('contact-form').addEventListener('submit', e => {
+  e.preventDefault();
+  const contactForm = document.getElementById('contact-form');
+  const formMessage = document.getElementById('form-message');
+  const formData = document.querySelectorAll('.form-data');
+
+  const obj = {};
+
+  formData.forEach(formElement => {
+    obj[formElement.name] = formElement.value;
+  });
+  fetch(
+    'https://script.google.com/macros/s/AKfycbxG4MzUf6Z6X5KxZbA66j0CiARk6ilV8qcsUhqA2HNw7tzJ81-E/exec',
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify(obj)
+    }
+  )
+    .then(response => response.text())
+    .then(response => {
+      console.log(response);
+      formMessage.textContent = `Message successfully sent.`;
+      formMessage.style.color = 'green';
+    })
+    .catch(err => {
+      console.log('Error: ', err);
+      formMessage.textContent = `Failed to send message. Please check your connection`;
+      formMessage.style.color = 'red';
+      return false;
+    });
+});
